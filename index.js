fa715ed0d9f79a6bbfc3aadea29a3cbe457cc92e
randa yahya <randaalrayan96@gmial.com>
2020-07-30 23:47:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, str){
+  let result = drivers.filter(driver => driver.toLowerCase() === str.toLowerCase())
+  return result
+}
+function fuzzyMatch(drivers, str){
+
+  let result = drivers.filter(driver => driver.slice(0, str.length) === str )
+  return result
+}
+
+function matchName(drivers, name){
+  let result = drivers.filter(driver => driver.name === name)
+  return result;
+}
